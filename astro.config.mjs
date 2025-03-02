db55ed3e21f15d5cc385bd4a13bf2405d219113a
Ravi Teja <raviteja155@gmail.com>
2025-03-02 18:52:52 +0530

    Revamp homepage.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,10 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bolty.in',
-	integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap()],
+  vite: {
+    css: {
+      transformer: "scss",
+    },
+  },
 });
