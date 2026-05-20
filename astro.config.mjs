f9b57885a4c76bb3da78536f54800e1e7484cc10
Ravi Teja <raviteja155@gmail.com>
2026-05-20 11:39:20 +0530

    Migrate to Astro 6. Add images to services.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bolty.in',
+	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
